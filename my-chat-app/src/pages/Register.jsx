2e13c3476eb0d3bf6b7d99a484d3a4c8f91d7e57
Waydon375 <saalwaydon@gmail.com>
2024-01-31 11:16:02 +0200

    #

--- my-chat-app/src/pages/Register.jsx
+++ my-chat-app/src/pages/Register.jsx
@@ -4,13 +4,14 @@ const Register = () => {
   return (
     <div className='formContainer'>
     <div className='formWrapper'>
-    <span className="logo">W Chat</span>
+    <span className="logo">Chatier</span>
     <span className="title">Register</span>
         <form>
             <input type='text ' placeholder='display name'/>
             <input type='email' placeholder="email" />
             <input type='password' placeholder="password"/>
-            <input type="file"/>
+            <input style={{display:'none'}}type="file" id='file'/>
+            <label style={{cursor:'pointer'}} htmlFor='file'>choose file</label>
             <button>Sign up</button>
         </form>
         <p>Do you already have a account? Login</p>
