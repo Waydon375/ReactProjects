82143b690997b41a1f47a2ce24d6d6596e669a51
Waydon375 <saalwaydon@gmail.com>
2024-01-31 12:38:23 +0200

    #

--- my-chat-app/src/pages/Login.jsx
+++ my-chat-app/src/pages/Login.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
+  <div>
+    <div className='formContainer'>
+    <div className='formWrapper'>
+    <span className="logo">Chattier</span>
+    <span className="title">Login</span>
+        <form>
+            <input type='email' placeholder="email" />
+            <input type='password' placeholder="password"/>
+ 
+            <button>Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
     </div>
+    </div>
+  </div>
   )
 }
 
--- my-chat-app/src/pages/Register.jsx
+++ my-chat-app/src/pages/Register.jsx
@@ -4,17 +4,24 @@ const Register = () => {
   return (
     <div className='formContainer'>
     <div className='formWrapper'>
-    <span className="logo">Chatier</span>
+    <span className="logo">Chattier</span>
     <span className="title">Register</span>
         <form>
             <input type='text ' placeholder='display name'/>
             <input type='email' placeholder="email" />
             <input type='password' placeholder="password"/>
             <input style={{display:'none'}}type="file" id='file'/>
-            <label style={{cursor:'pointer'}} htmlFor='file'>choose file</label>
+            <label style={{cursor:'pointer'}} htmlFor='file'>
+              <img src='src/assets/add.png' alt=''/>
+              <span>Add your avatar</span>
+            </label>
             <button>Sign up</button>
         </form>
-        <p>Do you already have a account? Login</p>
+        <p>Do you already have a account? 
+        <a href='#'>
+        Login
+        </a>
+        </p>
     </div>
 </div>
    
