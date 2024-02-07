f7530bd86e2f53e2538fb011ce198fd2355a4549
Waydon375 <saalwaydon@gmail.com>
2024-02-07 12:48:15 +0200

    #

--- my-chat-app/src/components/Input.jsx
+++ my-chat-app/src/components/Input.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Img from "../assets/img.png"
+import Attach from "../assets/attach.png"
 
 const input = () => {
   return (
-    <div>
-      Input
+    <div className='input'>
+        <input type="text" placeholder='Type something...'/>
+        <div className="send">
+          <img src={Attach} alt="" />
+          <input type='file' style={{display:"none"}} id='file'/>
+          <label htmlFor='file'>
+            <img src={Img} alt="" />
+          </label>
+          <button>Send</button>
+        </div>
     </div>
   )
 }
--- my-chat-app/src/components/Message.jsx
+++ my-chat-app/src/components/Message.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className='message'>
+      message  
     </div>
   )
 }
